edfc1de9bccd7f779716ba056534472cf70414b4
Minne Atairu <79331776+minneatairu@users.noreply.github.com>
2024-06-03 12:20:20 -0400

    d

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ document.addEventListener("DOMContentLoaded", function () {
     var modal = document.getElementById("myModal");
 
     // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
+    var btn = document.getElementById("openModalBtn");
 
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
 
-    // When the user clicks the button, open the modal
+    // When the user clicks on the button, open the modal
     btn.onclick = function() {
         modal.style.display = "block";
     }
