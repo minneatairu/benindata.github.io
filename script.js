bdc219c72502301eb006d11c68f8f454f0c3b09d
Minne Atairu <79331776+minneatairu@users.noreply.github.com>
2024-06-03 12:11:17 -0400

    d

--- script.js
+++ script.js
@@ -7,4 +7,14 @@ document.addEventListener("DOMContentLoaded", function () {
         const clone = image.cloneNode(true);
         grid.appendChild(clone);
     });
+        // Get the modal
+        var modal = document.getElementById("myModal");
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+    
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
 });
